21465c0b41e975d070ec9a0bb026f0662fd0cbe5
cesarnavarro1011 <cesarnavarro1011+github@gmail.com>
2023-02-10 19:31:09 -0500

    add react-navegation

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/infrastructure/components/layaut/header';
 import Home from "./src/infrastructure/view/home";
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Header/>
-      <Home/>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Header/>
+        <Home/>
+      </View>
+    </NavigationContainer>
   );
 }
 
